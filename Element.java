dfca43228a4ee6a0ebfb99c7b4f34b6641a12bc3
Sammy Burns <sburns2023@kehillahstudent.org>
2022-08-30 23:08:52 -0700

    first test case with private vars

--- Element.java
+++ Element.java
@@ -1,1 +1,20 @@
+public class Element{
 
+    private Element prev;
+    private Element next;
+    private int key;
+
+
+    public Element(int _key){
+
+
+        key = _key;
+
+    }
+
+    public int getkey(){
+
+        return key;
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
+import javax.xml.transform.Templates;
+
+public class Main{
+    public static void main (String[] args){
+
+        System.out.println("Hello World");
+
+        Element fe = new Element(3);
+
+        if(testing()) pass++;
+
+        else fail++;
+
+
+        System.out.println("passed: " + pass);
+        System.out.println("failed: " + fail); 
+
+
+    }
+
+    static int pass = 0;
+    static int fail = 0;
+
+
+    public static boolean testing(){
+        Element fe = new Element(3);
+        return fe.getkey() == 3;
+    }
+}
 
